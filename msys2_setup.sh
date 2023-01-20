736c0046e973a73180a70a2692f5a9e8d2f8ad9c
viayulo <vvvhope45@gmail.com>
2023-01-20 02:39:03 -0800

    update msys2 package list

---
+++ msys2_setup.sh
@@ -0,0 +1,30 @@
+pacman -Suy
+
+pacman -S mingw-w64-clang-x86_64-emacs
+pacman -S mingw-w64-clang-x86_64-rust
+pacman -S mingw-w64-clang-x86_64-rust-docs
+pacman -S mingw-w64-clang-x86_64-openocd
+pacman -S mingw-w64-clang-x86_64-gnupg
+pacman -S mingw-w64-clang-x86_64-aspell
+pacman -S mingw-w64-clang-x86_64-aspell-en
+pacman -S mingw-w64-clang-x86_64-openssl
+pacman -S mingw-w64-clang-x86_64-python
+pacman -S mingw-w64-clang-x86_64-python-setuptools
+pacman -S mingw-w64-clang-x86_64-ffmpeg
+pacman -S mingw-w64-clang-x86_64-openxr-sdk
+
+# Group
+pacman -S mingw-w64-clang-x86_64-texlive-full
+pacman -S mingw-w64-clang-x86_64-toolchain
+pacman -S mingw-w64-clang-x86_64-vulkan-devel
+pacman -S mingw-w64-clang-x86_64-graphviz
+pacman -S mingw-w64-clang-x86_64-git-lfs
+pacman -S mingw-w64-clang-x86_64-mpv
+
+#openbsd-netcat
+#net-utils
+#sys-utils
+#mingw-w64-clang-x86_64-blender
+#mingw-w64-clang-x86_64-postgresql
+
+#pacman -Qe
